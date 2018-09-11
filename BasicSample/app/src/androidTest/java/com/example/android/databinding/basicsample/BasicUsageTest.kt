3da311c9d1430faaff9dbcd9f372d3bfa4370bc7
Jose Alcérreca <JoseAlcerreca@users.noreply.github.com>
2018-09-11 18:03:02 +0200

    Bumps versions to gradle 4.4 and AGP 3.1.4 (#11)

--- BasicSample/app/src/androidTest/java/com/example/android/databinding/basicsample/BasicUsageTest.kt
+++ BasicSample/app/src/androidTest/java/com/example/android/databinding/basicsample/BasicUsageTest.kt
@@ -19,7 +19,9 @@ package com.example.android.databinding.basicsample
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.example.android.databinding.basicsample.ui.MainActivity
